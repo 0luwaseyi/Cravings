c5d0fcd2f561a941e28575430f71c4a6b1230288
Oluwaseyi <oluseyitimilehin3@gmail.com>
2024-05-18 15:40:27 -0700

    corrected major components

--- src/Components/Order.tsx
+++ src/Components/Order.tsx
@@ -4,13 +4,13 @@ import Image  from "next/image";
 const Order =()=>{
     return(
         <div>
-            <div className="w-full h-[190vw] lg:h-[30.5vw]">
-                <Image src = "/blackback.svg" alt="background" layout="responsive" objectFit="cover" width={100} height={100}/>
-            </div>
+            <div className="relative w-full h-[190vw] lg:h-[30.5vw]">
+                <Image src = "/blackback.svg" alt="background" layout="fill" objectFit="cover"/>
 
-            <div className="flex flex-col lg:flex-row mt-[70vw] lg:mt-[-25vw] ml-[8vw] space-x-[10vw]">
-            <div className="flex flex-col space-y-4 w-[36vw]">
-                <h1 className="font-bold text-[#000] lg:text-[#fff] text-2xl">Place an order with our mobile app</h1>
+                <div className="absolute inset-0 flex flex-col space-y-3 space-x-0 lg:flex-row ml-[8vw] lg:space-x-[10vw] lg:space-y-0">
+            <div className="w-[36vw]">
+                <div className="w-[85vw] lg:w-[40vw] overflow-hidden text-center lg:text-left mx-auto pt-5 lg:pt-[7vw] flex flex-col space-y-2">
+                <h1 className="font-bold text-[#fff] lg:text-[#fff] text-2xl">Place an order with our mobile app</h1>
                 <p className="text-[#fff] text-sm">Why wait in line or stress about dinner plans when you can have a gourmet experience delivered
                     straight to your doorstep? Our mobile app makes it convenient for you to explore our diverse menu, customize
                     your order and track in real time.</p>
@@ -18,17 +18,23 @@ const Order =()=>{
                         making your transaction hassle free.
                     </p>
 
-                    <div className="flex flex-row space-x-3">
+                    <div className="flex flex-row space-x-3 justify-center align-items items-center lg:justify-start">
                         <Image src ="/secondapple.svg" alt="applebutton" width={120} height={80}/>
                         <Image src ="/secondgoogle.svg" alt="googlebutton" width={120} height={80}/>
                     </div>
+
+                </div>
+               
             </div>
 
-            <div>
+            <div className="lg:pt-[5.6vw]">
                 <Image src="/secondphone.svg" alt="iphone" width={258} height={200}/>
             </div>
 
             </div>
+          
+            </div>
+
           
         </div>
 
